086c0efaf9e0bf807c96a69f2603f7f32e65b99b
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-16 22:57:25 -0500

    UPDATE: creating the socket and establish connection with frontend

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,7 +1,25 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require('http').Server()
+const io = require('socket.io')(server)
+const port = require('./config').SERVER_PORT
+
+const banner = `
+*********************************************
+*       NodeJS connection with Sockets      *
+*           By: davidle.codes               *
+*           Tic-Tac-Toe Server              *
+* ----------------------------------------- *
+*  Status: Online                           *
+*  Server port: ${port}                        *
+*  Frontend link: http://localhost:${port}     * 
+*********************************************
+`
+
+io.on('connection', socket => {
+    socket.on('register', user => {
+        console.info(`User registered: ${user.name}`)
+    })
+})
+
+server.listen(port, () => {
+    console.log(banner)
+})
