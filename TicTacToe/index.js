eb4a233dd8352182e7125769f080955ab6027ea2
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-17 13:40:07 -0500

    UPDATE: importing the backend connection

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,6 +1,7 @@
 const server = require('http').Server()
 const io = require('socket.io')(server)
 const port = require('./config').SERVER_PORT
+require('./backend/connection')(io)
 
 const banner = `
 *********************************************
@@ -14,11 +15,6 @@ const banner = `
 *********************************************
 `
 
-io.on('connection', socket => {
-    socket.on('register', user => {
-        console.info(`User registered: ${user.name}`)
-    })
-})
 
 server.listen(port, () => {
     console.log(banner)
